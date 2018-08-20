e32e1195bb58e8cfe6d3d221f190c66b7956c598
suhas023 <notsuhas@gmail.com>
2018-08-21 00:09:03 +0530

    added missing functionality

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
-frontend-nanodegree-arcade-game
-===============================
+# Arcade Game
+ A game in which you have to avoid all the spawning enimies to reach your destination.
 
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
+ # How to Run
+ 1. Clone this repository.
+ 2. Run index.html and you're done. Have fun.
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+# How to Play
+- Wait till game loads.
+- Use arrow keys to move the player.
+- The goal is to reach the water stream at top.
+- To reset after raching the goal, press up-arrow key on the edge the stream.
+
+# Screenshot
+![sample shot](/screenshot/arcade.png?raw=true)
--- js/app.js
+++ js/app.js
@@ -2,7 +2,9 @@
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.speed = Math.floor(Math.random() * 100) + 100;
+    this.x = Math.floor(Math.random() * (-183)) - 83;
+    this.y = Math.random() * 225 + 25;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -14,6 +16,18 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    let newX = this.x + this.speed * dt;
+    // When enemy moves outside reset the enemy with new values
+    if (newX >= 498) {
+        this.x = -83;
+        // get new y coordinat and speed for the enemy
+        this.speed = Math.floor(Math.random() * 100) + 125;
+        this.y = Math.random() * 225 + 25;
+    }
+    else
+        this.x =  newX;
+    if((Math.abs(this.x - player.x) <= 30) && (Math.abs(this.y - player.y) <= 42))
+        player.reset();
 };
 
 // Draw the enemy on the screen, required method for game
@@ -25,11 +39,52 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function() {
+    this.x = 202;
+    this.y = 415;
+    this.sprite = 'images/char-boy.png';
+}
+
+// to get render method method from Enemy
+Player.prototype = Object.create(Enemy.prototype);
+Player.prototype.constructor = Player;
+
+Player.prototype.update = function() {
+    // show winner banner if the player reaches the top
+    if(this.y < 23) {
+        winner.style.visibility = "visible";
+    }
+
+    // reset the game if player jums of the top.
+    if(this.y < -40) {
+        this.reset();
+    }
+}
+
+Player.prototype.reset = function() {
+    //hide the winner banner
+    winner.style.visibility = "hidden";
+    this.y = 414;
+    this.x = 202;
+}
+
+Player.prototype.handleInput = function(key) {
+    switch(key) {
+        case 'left':    this.x -= (this.x > 1) ? 25 : 0;                break;
+        case 'right':   this.x += (this.x < 410) ? 25 : 0;              break;
+        case 'up':      this.y -= 30;                                   break;
+        case 'down':    this.y += (this.y < 400)? 30:0;                 break;
+    }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+
+
+var allEnemies = [new Enemy(), new Enemy(), new Enemy(), new Enemy(), new Enemy(), new Enemy(), new Enemy(), new Enemy()];
+var player = new Player()
 
 
 // This listens for key presses and sends the keys to your
@@ -44,3 +99,7 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+// get winner banner
+winner = document.getElementsByTagName('h1')[0];
+
